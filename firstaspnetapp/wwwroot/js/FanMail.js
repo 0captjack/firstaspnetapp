5c7137d0055fb73725be705889d65a77f0dea488
0captjack <85979485+0captjack@users.noreply.github.com>
2022-01-09 21:31:40 -0700

    Add project files.

---
+++ firstaspnetapp/wwwroot/js/FanMail.js
@@ -0,0 +1,10 @@
+﻿$('#btnworked').click(function () {
+    alert('From: ' + $('#fromTxt').val() +
+        ', Subject: ' + $('#subjectTxt').val() +
+        ', Message: ' + $('#messageTxt').val());
+
+
+    $('#photoAnderson').fadeToggle('slow');
+
+
+})
